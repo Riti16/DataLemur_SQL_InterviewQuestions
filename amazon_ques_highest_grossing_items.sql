2ee40467408dd2f3695f8b084fde7fa60b69cf66
Riti Dass <37213335+Riti16@users.noreply.github.com>
2024-03-20 21:20:11 +0530

    Update amazon_ques_highest_grossing_items.sql

--- amazon_ques_highest_grossing_items.sql
+++ amazon_ques_highest_grossing_items.sql
@@ -15,12 +15,14 @@ appliance	washing machine	123	219.80	03/02/2022 12:00:00
 electronics	vacuum	178	152.00	04/05/2022 12:00:00
 electronics	wireless headset	156	249.90	07/08/2022 12:00:00
 electronics	vacuum	145	189.00	07/15/2022 12:00:00
+  
 Example Output:
 category	product	total_spend
 appliance	refrigerator	299.99
 appliance	washing machine	219.80
 electronics	vacuum	341.00
 electronics	wireless headset	249.90
+  
 Explanation:
 Within the "appliance" category, the top two highest-grossing products are "refrigerator" and "washing machine."
 
