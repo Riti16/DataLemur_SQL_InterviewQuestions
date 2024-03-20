c58ea7e0bad588979d91f2a16658256bce69759d
Riti Dass <37213335+Riti16@users.noreply.github.com>
2024-03-20 21:16:59 +0530

    Update facebook_ques_active_user_retention.sql

--- facebook_ques_active_user_retention.sql
+++ facebook_ques_active_user_retention.sql
@@ -1,8 +1,7 @@
 Assume you're given a table containing information on Facebook user actions. Write a query to obtain number of monthly active users (MAUs) in July 2022, including the month in numerical format "1, 2, 3".
 
-Hint:
+Hint: An active user is defined as a user who has performed actions such as 'sign-in', 'like', or 'comment' in both the current month and the previous month.
 
-An active user is defined as a user who has performed actions such as 'sign-in', 'like', or 'comment' in both the current month and the previous month.
 user_actions Table:
 Column Name	Type
 user_id	integer
@@ -16,9 +15,11 @@ user_id	event_id	event_type	event_date
 445	3634	like	06/05/2022 12:00:00
 742	1374	comment	06/05/2022 12:00:00
 648	3124	like	06/18/2022 12:00:00
+  
 Example Output for June 2022:
 month	monthly_active_users
 6	1
+  
 Example
 In June 2022, there was only one monthly active user (MAU) with the user_id 445.
 
